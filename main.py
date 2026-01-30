92759e54588bbe278bf65c87f4ff8a50e3d37d6c
RenatTest <renattesttask@gmail.com>
2026-01-30 20:24:29 +0200

    feat(PL-1): variables, data types

---
+++ main.py
@@ -0,0 +1,13 @@
+message = "Lets play" # змінна типу str
+age = 39 # змінна типу int
+pi = 3.14 # змінна типу float
+signs = ["X", "O", "O", "X",] # змінна типу list
+players = {1: "X", 2: "O"} # змінна типу dict;  players[1] - get value
+
+# Створити змінну на основі іншої
+# message2 = message
+# id(message) = id(message2) - це одна і та ж сама комірка в пам'яті
+
+# Називати змінні
+# snake_case
+# змінна іменник
