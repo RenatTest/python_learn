056bdf5a23a0a5810987a99b3551dd725d40aa2a
RenatTest <renattesttask@gmail.com>
2026-02-20 20:49:08 +0200

    feat(PL-2): format str, special symbols

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# ---------- Lesson 1 Variables ----------
+
 message = "Lets play" # змінна типу str
 age = 39 # змінна типу int
 pi = 3.14 # змінна типу float
@@ -11,3 +13,34 @@ players = {1: "X", 2: "O"} # змінна типу dict;  players[1] - get value
 # Називати змінні
 # snake_case
 # змінна іменник
+
+# ---------- Lesson 2 Format str, special symbols ----------
+
+message2 = "Player X won!"
+message3 = 'Player X won!'
+
+# multi line string
+
+message4 = """ 
+Player X won!
+Congrats!
+"""
+
+message5 = "Player X won!\nCongrats!"
+
+# dynamic string, formatted string
+
+winner = "Renata"
+loser = "Pasha"
+message6 = f"Player {winner} won! Congrats!\nPlayer {loser} lost! Foo!"  
+
+# Const
+
+MENU = """
+
+Hello! Welcome to tic tac toe game!
+Rules: X and O takes turns on the grid 3x3
+
+"""
+
+print(MENU)
