5c356f2cb26a1ccfe4ef29a72734b9d36f1d6516
RenatTest <renattesttask@gmail.com>
2026-02-28 17:42:02 +0200

    feat(PL-3): casting types

--- main.py
+++ main.py
@@ -52,13 +52,63 @@ PLAYER_WON_MESSAGE = f'Player {player}, you won!'
 # ---------- Lesson 3 input() ----------
 
 # control + C - end of program
-winner = input('Winner enter your sign >>> ') 
-loser = input('Loser enter your sign >>> ') 
+# winner = input('Winner enter your sign >>> ') 
+# loser = input('Loser enter your sign >>> ') 
 
 message = f"Player {winner} won! Congrats! Player {loser} lost! Foo!"  
 
-print(message)
-print(type(winner)) # str always
+# print(message)
+# print(type(winner)) # str always
 
-sign = input('Please enter your sign:')
-print(f'Player {sign}, your turn!')
+# sign = input('Please enter your sign:')
+# print(f'Player {sign}, your turn!')
+
+# ---------- Lesson 4 casting types ----------
+
+SIGNS = {
+    1: "X",
+    2: "0",
+}
+
+# convert str to int - function int()
+# Додавання рядків - конкатенація
+# Множення рядків - "22" * 2 = '2222'
+
+winner_sign = int(input('Please choose a sign [1: X, 2: O] >>> ')) 
+print(winner_sign)
+print(type(winner_sign))
+message_winner = f'Player {SIGNS[winner_sign]} is winner'
+print(message_winner)
+
+# convert int to str - function str()
+int_from_str = str(88)
+print(int_from_str)
+print(type(int_from_str))
+
+# convert str to float - float()
+str_to_float = float("88.88")
+print(str_to_float)
+print(type(str_to_float))
+
+# True and False
+age = 20
+is_adult = age > 18
+is_child = age < 18
+print(is_adult)
+print(is_child)
+# 0 - False, number not 0 - True
+# bool("str") - True
+# bool("") - False
+
+# None
+name = None
+print(name)
+
+# Залишок від ділення %
+turn_number = 5
+print(turn_number % 2 == 0) # False
+print(turn_number % 2 == 1) # True
+
+# Prctice
+choice = int(input("Player 1, what's your sign [choose 1:X or 2:O]: "))
+print(choice % 2 == 0)
